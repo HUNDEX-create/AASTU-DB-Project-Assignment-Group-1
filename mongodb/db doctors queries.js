f1fd975793daaef0bfaf8f05d498a30de2d0bac1
Gemechu Fekadu <fekadunathan372@gmail.com>
2026-05-13 15:27:01 -0700

    Add initial doctor records to database

---
+++ mongodb/db doctors queries.js
@@ -0,0 +1,10 @@
+db.doctors.insertMany([
+  {
+    "_id": "D001", "name": "Dr. Mekonnen Tadesse", "specialty": "Cardiology",
+    "phone": "+251911876543", "experienceYears": 12
+  },
+  {
+    "_id": "D002", "name": "Dr. Selamawit Girma", "specialty": "Pediatrics",
+    "phone": "+251922987654", "experienceYears": 8
+  }
+]);
